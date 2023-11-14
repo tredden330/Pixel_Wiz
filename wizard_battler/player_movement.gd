f644afdd3deb2fb2ad4749a8c525ed097d76b1a7
tredden330 <tom@thomasredden.com>
2023-11-13 22:09:55 -0500

    change fireball

--- wizard_battler/player_movement.gd
+++ wizard_battler/player_movement.gd
@@ -11,7 +11,7 @@ func _ready():
 	print(get_children())
 	
 	var fireballs = []
-	for i in range(20):
+	for i in range(200):
 		var instance = fireball.instantiate()
 		instance.position = Vector2(rng.randf_range(0.0, 1000.0),rng.randf_range(0.0, 500.0))
 		fireballs.append(instance)
@@ -23,11 +23,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("move_up_1"):
+	if Input.is_action_pressed("up"):
 		position.y -= speed
-	elif Input.is_action_pressed("move_down_1"):
+	if Input.is_action_pressed("down"):
 		position.y += speed
+	if Input.is_action_pressed("left"):
+		position.x -= speed
+	if Input.is_action_pressed("right"):
+		position.x += speed
 	
-func _input(event):
-	if event.is_action_pressed("move_up_1"):
-		print("my_action occurred!")
+#func _input(event):
+#	if event.is_action_pressed("move_up_1"):
+#		print("my_action occurred!")
